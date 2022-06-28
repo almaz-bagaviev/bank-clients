c9e5cc0b2c01c35c308538885e73beae5c3b5403
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-06-28 21:50:20 +0300

    Remove summaries

--- BankClients/Entities/Clients.cs
+++ BankClients/Entities/Clients.cs
@@ -2,44 +2,14 @@
 
 public struct Client
 {
-    // <summary>
-    /// ID
-    /// </summary>
     public int Id { get; set; }
-
-    /// <summary>
-    /// Дата и время добавления записи
-    /// </summary>
     public DateTime Date { get; set; }
-
-    /// <summary>
-    /// Ф.И.О сотрудника
-    /// </summary>
     public string FullName { get; set; }
-
-    /// <summary>
-    /// Возраст
-    /// </summary>
     public int Age { get; set; }
-
-    /// <summary>
-    /// Рост
-    /// </summary>
     public int Growth { get; set; }
-
-    /// <summary>
-    /// Дата рождения
-    /// </summary>
     public DateTime DateOfBirth { get; set; }
-
-    /// <summary>
-    /// Место рождения
-    /// </summary>
     public string PlaceOfBirth { get; set; }
 
-    /// <summary>
-    /// Конструктор Worker
-    /// </summary>
     public Client(int id, DateTime date, string fullName, int age, int growth, DateTime dateOfBirth, string placeOfBirth)
     {
         this.Id = id;
