0b5ae4552da7feb36cbfabd57d17dd83ac065953
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-17 23:28:02 +0300

    git add usings for Db

--- BankClients/Usings.cs
+++ BankClients/Usings.cs
@@ -2,3 +2,6 @@
 global using BankClients.Repositories;
 global using BankClients.Model;
 global using static System.Console;
+global using Microsoft.EntityFrameworkCore;
+global using BankClients.Contexts;
+global using System.Text;
