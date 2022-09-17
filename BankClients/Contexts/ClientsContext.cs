899a0752ee703db384a223c674656108b5cf32da
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-17 23:28:40 +0300

    add Db

---
+++ BankClients/Contexts/ClientsContext.cs
@@ -0,0 +1,18 @@
+﻿namespace BankClients.Contexts;
+
+public class ClientsContext: DbContext
+{
+    public DbSet<Client> ClientTable { get; set; }
+
+    public ClientsContext()
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server = (LocalDB)\MSSQLLocalDB;
+                                    DataBase = BankCliensDataBase;
+                                    Trusted_Connection = True;");
+    }
+}
